5fe60a1c0c7a05d5c2300ece08fffa067038951c
peshmamtaib <md.taib21@gmail.com>
2023-06-15 15:41:25 +0530

    Created a Multiplication of two numbers

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+class Multiplication
+{
+	public static void main(String [] args)
+	{
+		int n1,n2,prod=0;
+		n1=20;
+		n2=30;
+		prod=n1*n2;
+		System.out.println("The Product is :+"prod);
+	}
+}
+
